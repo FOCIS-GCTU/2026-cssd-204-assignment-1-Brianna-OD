1a06acc21ee49749e6847111f8ac733f7da40e6f
Ofosu Dankyi Brianna Akosua Ofosua <2425403183@live.gctu.edu.gh>
2026-05-31 11:21:47 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 2
+#
+# Name: Brianna Akosua Ofosua Ofosu Dankyi
+# STUDENT ID:  2425403183
+# Email: 2425403183@live.gctu.edu.gh
+# Grader: Emma
+#
+# On my honor, Brianna Akosua Ofosua Ofosu Dankyi, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Prints initials BAO in large block letters using only print() and strings
+
+    print()
+    print("...BAO")
+    print()
+    print("...BBBBBBBBBBBB...****......AAAAAA......****...OOOOOOOOOOOO")
+    print("...B..........B...****.....A......A.....****...O..........O")
+    print("...B..........B...****....A........A....****...O..........O")
+    print("...B..BBBBBB..B...****...A..........A...****...O..........O")
+    print("...B..B....B..B...****...AAAAAAAAAAAA...****...O..........O")
+    print("...B..BBBBBB..B...****...A..........A...****...O..........O")
+    print("...B..........B...****...A..........A...****...O..........O")
+    print("...B..........B...****...A..........A...****...O..........O")
+    print("...B..........B...****...A..........A...****...O..........O...****")
+    print("...BBBBBBBBBBBB...****...A..........A...****...OOOOOOOOOOOO...****")
+    print()
 
 
-
-main()
+if __name__ == "__main__":
+    main()
